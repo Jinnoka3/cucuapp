e0cb97a000a2dc3dfe1637b5fb75d71c812ce3c4
daria.kalinina <galinaprokofevna@gmail.com>
2019-08-22 16:16:34 +0400

    for misha

--- src/test/java/seleniumgluecode/RegistrationTest.java
+++ src/test/java/seleniumgluecode/RegistrationTest.java
@@ -10,7 +10,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 
-public class Test {
+public class RegistrationTest {
 
     public static WebDriver driver;
     @Given("user is on homepage")
@@ -23,10 +23,10 @@ public class Test {
         driver.get("http://automationpractice.com/index.php");
     }
 
-    @When("user navigates to Login Page")
+   /* @When("user navigates to Login Page")
     public void user_navigates_to_Login_Page() throws Throwable {
         driver.findElement(By.linkText("Sign in")).click();
-    }
+    }*/
 
     @When("user enters username and Password")
     public void user_enters_username_and_Password() throws Throwable {
